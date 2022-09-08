cfbae2c17350fc2a4b4a39d59be0fd0b730ba05e
Yuri3-xr <316200668@qq.com>
2022-09-08 18:33:50 +0800

    Mf_Sieve

---
+++ Number_Theory/Mf_Sieve.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Prime_Sieve.hpp"
+
+template <class T>
+vector<T> mf_sieve(int n, function<T(i64, i64, i64)> f) {
+    /*
+        ##pragma f is a multiplicative-function
+        f(n,p,c) <-> n=p^c
+    */
+    vector<T> ans(n + 1, T());
+    vector<int> ps = prime_sieve(n);
+    int p(ps.size());
+
+    function<void(int, i64, T)> dfs = [&](int i, i64 x, T y) {
+        ans[x] = y;
+        if (y == T()) return;
+        for (int j = i + 1; j < p; j++) {
+            i64 nx = x * ps[j];
+            i64 dx = ps[j];
+            if (nx > n) break;
+            for (int c = 1; nx <= n; nx *= ps[j], dx *= ps[j], ++c)
+                dfs(j, nx, y * f(dx, ps[j], c));
+        }
+    };
+    ans[1] = 1;
+    dfs(-1, 1, 1);
+    return ans;
+};
