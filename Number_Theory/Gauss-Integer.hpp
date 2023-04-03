5ff3ad7a7fcf61a6968a8cd1a669b1e086c51e93
yuri3 <48235548+Yuri3-xr@users.noreply.github.com>
2023-04-03 20:21:35 +0800

    code more clear

--- Number_Theory/Gauss-Integer.hpp
+++ Number_Theory/Gauss-Integer.hpp
@@ -19,8 +19,8 @@ struct G {
         return {a.a * b.a - a.b * b.b, a.a * b.b + a.b * b.a};
     }
     bool operator==(const G &x) const { return x.a == a && x.b == b; }
-    G operator*(const i128 &t) const { return {a * t, b * t}; }
-    G operator/(const i128 &t) const { return {a / t, b / t}; }
+    inline G operator*(const i128 &t) const { return {a * t, b * t}; }
+    inline G operator/(const i128 &t) const { return {a / t, b / t}; }
     G friend operator/(const G &a, const G &b) {
         i128 len = b.a * b.a + b.b * b.b;
         G c = a * G(b.a, -b.b);
@@ -30,23 +30,22 @@ struct G {
         };
         return {div(2 * c.a + len, 2 * len), div(2 * c.b + len, 2 * len)};
     }
+    G power(i128 b) {
+        G res(1, 0);
+        G a = *this;
+        for (; b; b /= 2, a = a * a) {
+            if (b % 2) {
+                res = res * a;
+            }
+        }
+        return res;
+    }
 };
 
-static G one = G(1, 0);
-template <class T>
-T _power(T x, i128 b) {
-    T res = one;
-    while (b) {
-        if (b & 1) res = res * x;
-        x = x * x;
-        b = b / 2;
-    }
-    return res;
-}
 G solveprime(i128 p) {
     if (p == 2) return {1, 1};
     i128 t = 1;
-    auto qpow = [&](i128 a, i128 b, i128 p) {
+    auto qpow = [](i128 a, i128 b, i128 p) {
         i128 res = 1;
         while (b) {
             if (b & 1) res = res * a % p;
@@ -58,13 +57,13 @@ G solveprime(i128 p) {
     for (; qpow(t, (p - 1) / 2, p) != p - 1;) t++;
     i128 k = qpow(t, (p - 1) / 4, p);
 
-    std::function<G(G, G)> Ggcd = [&](G a, G b) -> G {
+    auto gcd = [&](auto &&self, G a, G b) -> G {
         if (b.a == 0 && b.b == 0)
             return a;
         else
-            return Ggcd(b, a - (a / b) * b);
+            return self(self, b, a - (a / b) * b);
     };
-    auto g = Ggcd({p, 0}, {k, 1});
+    auto g = gcd(gcd, {p, 0}, {k, 1});
 
     if (g.a < 0) g.a = -g.a;
     if (g.b < 0) g.b = -g.b;
@@ -79,7 +78,7 @@ std::vector<G> solvecomposite(i128 n) {
         if (p % 4 == 1) {
             G A = solveprime(p);
             G B = {A.a, -A.b};
-            auto now = _power<G>(A, 2 * tmp);
+            auto now = A.power(2 * tmp);
 
             std::vector<G> res;
             for (i64 i = 0; i <= 2 * tmp; i++) {
@@ -89,7 +88,7 @@ std::vector<G> solvecomposite(i128 n) {
             swap(v, res);
         } else {
             G now(p, 0);
-            now = _power<G>(now, tmp);
+            now = now.power(tmp);
             for (auto &&it : v) it = it * now;
         }
     }
@@ -103,7 +102,7 @@ std::vector<G> solvecomposite(i128 n) {
     v.resize(unique(v.begin(), v.end()) - v.begin());
 
     std::vector<G> t;
-    for (auto [a, b] : v)
+    for (const auto &[a, b] : v)
         if (a != 0 && b != 0) t.emplace_back(a, b);
     return t;
 }
